ba257880595fe90fd7b41bf636868537d55fb7ac
Chris <chris.g14@iiits.in>
2018-11-26 21:06:09 +0530

    Settings back to normal.

--- settings.py
+++ settings.py
@@ -6,6 +6,6 @@
 # DB_URL = "postgres+psycopg2" + "".join(url.split("postgres")[1::])
 
 # Local running on SQLite
-# DB_URL = 'sqlite:///database.db'
-DB_URL = 'sqlite:///preon_data.db'
+DB_URL = 'sqlite:///database.db'
+# DB_URL = 'sqlite:///preon_data.db'
 Key = "sgjngfdfg//23/=+342][234097824-1<><123><!@#$#%^]"
